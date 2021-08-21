6902b645e74ba332d959fbc5c94c57beeab00027
Ashutosh Dash <ashu.vampire13@gmail.com>
2021-08-21 14:28:55 +0530

    first commit, added landing page & home page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var typed = new Typed("#typed", {
+  strings: [
+    "Cryptocurrencies",
+    "Bitcoin",
+    "Ethereum",
+    "Dogecoin",
+    "and many more...",
+  ],
+  
+  smartBackspace: true, // Default value
+  typeSpeed: 50, 
+  backSpeed: 55, 
+  loop: true,
+  loopCount: Infinity,
+  startDelay: 0,
+  showCursor: false,
+});
